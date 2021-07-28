b43966e27ff2ee52c27f6220e40920d8b1b1d208
FN572 <109976723+FN572@users.noreply.github.com>
2021-07-28 10:24:27 +0000

    Update JStest.html

---
+++ gitsome/lib/github3/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+github3
+=======
+
+See http://github3.readthedocs.org/ for documentation.
+
+:copyright: (c) 2012-2016 by Ian Cordasco
+:license: Modified BSD, see LICENSE for more details
+
+"""
