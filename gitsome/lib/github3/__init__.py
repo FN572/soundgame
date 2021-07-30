792240fa819b6369eaa23cf7f419bf2f8ccfd4da
FN572 <109976723+FN572@users.noreply.github.com>
2021-07-30 08:53:17 +0000

    copy fixes

--- gitsome/lib/github3/__init__.py
+++ gitsome/lib/github3/__init__.py
@@ -35,4 +35,16 @@ __all__ = (
     'AuthenticationFailed',  'BadRequest', 'ForbiddenError', 'GitHub',
     'GitHubEnterprise', 'GitHubError', 'GitHubStatus',
     'MethodNotAllowed', 'NotAcceptable', 'NotFoundError', 'ServerError',
-    'UnprocessableEntity', 'authorize', 'login', '
+    'UnprocessableEntity', 'authorize', 'login', 'enterprise_login', 'emojis',
+    'gist', 'gitignore_template', 'create_gist', 'issue', 'markdown',
+    'octocat', 'organization', 'pull_request', 'followers_of', 'followed_by',
+    'public_gists', 'gists_by', 'issues_on', 'gitignore_templates',
+    'all_repositories', 'all_users', 'all_events', 'organizations_with',
+    'repositories_by', 'starred_by', 'subscriptions_for', 'rate_limit',
+    'repository', 'search_code', 'search_repositories', 'search_users',
+    'search_issues', 'user', 'zen',
+    # Metadata attributes
+    '__package_name__', '__title__', '__author__', '__author_email__',
+    '__license__', '__copyright__', '__version__', '__version_info__',
+    '__url__',
+)
