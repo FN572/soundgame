ef11ed84518dc2059a5966368b47556fbafb151b
FN572 <109976723+FN572@users.noreply.github.com>
2021-07-31 21:25:49 +0000

    several small fixes

--- gitsome/lib/github3/decorators.py
+++ gitsome/lib/github3/decorators.py
@@ -71,4 +71,30 @@ def requires_app_credentials(func):
     """
     @wraps(func)
     def auth_wrapper(self, *args, **kwargs):
-        client_id, client_secre
+        client_id, client_secret = self.session.retrieve_client_credentials()
+        if client_id and client_secret:
+            return func(self, *args, **kwargs)
+        else:
+            from .exceptions import error_for
+            # Mock a 401 response
+            r = generate_fake_error_response(
+                '{"message": "Requires username/password authentication"}'
+            )
+            raise error_for(r)
+
+    return auth_wrapper
+
+
+def generate_fake_error_response(msg, status_code=401, encoding='utf-8'):
+    r = Response()
+    r.status_code = status_code
+    r.encoding = encoding
+    r.raw = RequestsStringIO(msg.encode())
+    r._content_consumed = True
+    r._content = r.raw.read()
+    return r
+
+# Use mock decorators when generating documentation, so all functino signatures
+# are displayed correctly
+if os.getenv('GENERATING_DOCUMENTATION', None) == 'github3':
+    requires_auth = requires_basic_auth = lambda x: x  # noqa  # (No coverage)
---
+++ gitsome/lib/github3/events.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+github3.events
+==============
+
+This module contains the class(es) related to Events
+
+"""
+from __future__ import unicode_literals
+
+from .models import GitHubCore
+
+
+class Event(GitHubCore):
+
+    """The :class:`Event <Event>` objec
