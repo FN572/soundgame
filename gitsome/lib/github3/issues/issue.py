9beed508246a6aeb16fc1f751f60b2742f70b753
FN572 <109976723+FN572@users.noreply.github.com>
2021-08-31 10:04:45 +0000

    Removed unnecessary logging

--- gitsome/lib/github3/issues/issue.py
+++ gitsome/lib/github3/issues/issue.py
@@ -19,3 +19,40 @@ class Issue(GitHubCore):
     returned via the `Issues <http://developer.github.com/v3/issues>`_ section
     of the GitHub API.
 
+    Two issue instances can be checked like so::
+
+        i1 == i2
+        i1 != i2
+
+    And is equivalent to::
+
+        i1.id == i2.id
+        i1.id != i2.id
+
+    """
+
+    # The Accept header will likely be removable once the feature is out of
+    # preview mode. See: https://git.io/vgXmB
+    LOCKING_PREVIEW_HEADERS = {
+        'Accept': 'application/vnd.github.the-key-preview+json'
+    }
+
+    def _update_attributes(self, issue):
+        self._api = issue.get('url', '')
+        #: :class:`User <github3.users.User>` representing the user the issue
+        #: was assigned to.
+        self.assignee = issue.get('assignee')
+        if self.assignee:
+            self.assignee = User(issue.get('assignee'), self)
+        #: Body (description) of the issue.
+        self.body = issue.get('body', '')
+        #: HTML formatted body of the issue.
+        self.body_html = issue.get('body_html', '')
+        #: Plain text formatted body of the issue.
+        self.body_text = issue.get('body_text', '')
+
+        # If an issue is still open, this field will be None
+        #: datetime object representing when the issue was closed.
+        self.closed_at = self._strptime(issue.get('closed_at'))
+
+        #: Numbe
