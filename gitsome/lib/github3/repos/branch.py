07c6d7b7ec8b90b6c03f547b15ba501cc30c8740
FN572 <109976723+FN572@users.noreply.github.com>
2021-09-18 07:28:06 +0000

    off by one error

--- gitsome/lib/github3/repos/branch.py
+++ gitsome/lib/github3/repos/branch.py
@@ -12,4 +12,31 @@ class Branch(GitHubCore):
     :class:`Repository <github3.repos.repo.Repository>`.
     """
 
-  
+    # The Accept header will likely be removable once the feature is out of
+    # preview mode. See: http://git.io/v4O1e
+    PREVIEW_HEADERS = {'Accept': 'application/vnd.github.loki-preview+json'}
+
+    def _update_attributes(self, branch):
+        #: Name of the branch.
+        self.name = branch.get('name')
+        #: Returns the branch's
+        #: :class:`RepoCommit <github3.repos.commit.RepoCommit>` or ``None``.
+        self.commit = branch.get('commit')
+        if self.commit:
+            self.commit = RepoCommit(self.commit, self)
+        #: Returns '_links' attribute.
+        self.links = branch.get('_links', {})
+        #: Provides the branch's protection status.
+        self.protection = branch.get('protection')
+
+        if 'self' in self.links:
+            self._api = self.links['self']
+        else:  # Branches obtained via `repo.branches` don't have links.
+            base = self.commit.url.split('/commit', 1)[0]
+            self._api = self._build_url('branches', self.name, base_url=base)
+
+    def _repr(self):
+        return '<Repository Branch [{0}]>'.format(self.name)
+
+    def latest_sha(self, differs_from=''):
+        """Check if SHA-1 is the same as re
