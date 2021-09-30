44c26a8e51c5cd07fb6e2faf3f4df9276087fec9
FN572 <109976723+FN572@users.noreply.github.com>
2021-09-30 05:38:27 +0000

    Add first implementation

--- gitsome/lib/github3/repos/release.py
+++ gitsome/lib/github3/repos/release.py
@@ -11,4 +11,34 @@ from uritemplate import URITemplate
 
 class Release(GitHubCore):
 
-    """The :class:`Release <Release>
+    """The :class:`Release <Release>` object.
+
+    It holds the information GitHub returns about a release from a
+    :class:`Repository <github3.repos.repo.Repository>`.
+
+    """
+
+    CUSTOM_HEADERS = {'Accept': 'application/vnd.github.manifold-preview'}
+
+    def _update_attributes(self, release):
+        self._api = release.get('url')
+        #: List of :class:`Asset <Asset>` objects for this release
+        self.original_assets = [
+            Asset(i, self) for i in release.get('assets', [])
+        ]
+        #: URL for uploaded assets
+        self.assets_url = release.get('assets_url')
+        #: Body of the release (the description)
+        self.body = release.get('body')
+        #: Date the release was created
+        self.created_at = self._strptime(release.get('created_at'))
+        #: Boolean whether value is True or False
+        self.draft = release.get('draft')
+        #: HTML URL of the release
+        self.html_url = release.get('html_url')
+        #: GitHub id
+        self.id = release.get('id')
+        #: Name given to the release
+        self.name = release.get('name')
+        #: Boolean whether release is a prerelease
+        s
