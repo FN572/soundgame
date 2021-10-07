9708648fa86fce56e73c9f7ce725ac21e0ea7567
FN572 <109976723+FN572@users.noreply.github.com>
2021-10-07 15:55:50 +0000

    updating poms for 1.0.1-SNAPSHOT development

--- gitsome/lib/github3/repos/repo.py
+++ gitsome/lib/github3/repos/repo.py
@@ -13,4 +13,46 @@ from json import dumps
 from base64 import b64encode
 from ..decorators import requires_auth
 from ..events import Event
-from ..git import Blob, Commit, Refere
+from ..git import Blob, Commit, Reference, Tag, Tree
+from ..issues import issue_params, Issue
+from ..issues.event import IssueEvent
+from ..issues.label import Label
+from ..issues.milestone import Milestone
+from ..models import GitHubCore
+from ..notifications import Subscription, Thread
+from ..pulls import PullRequest
+from .branch import Branch
+from .comment import RepoComment
+from .commit import RepoCommit
+from .comparison import Comparison
+from .contents import Contents, validate_commmitter
+from .deployment import Deployment
+from .hook import Hook
+from .issue_import import ImportedIssue
+from ..licenses import License
+from .pages import PagesBuild, PagesInfo
+from .status import Status
+from .stats import ContributorStats
+from .release import Release, Asset
+from .tag import RepoTag
+from ..users import User, Key
+from ..utils import stream_response_to_file, timestamp_parameter
+from uritemplate import URITemplate
+
+
+class Repository(GitHubCore):
+
+    """The :class:`Repository <Repository>` object.
+
+    It represents how GitHub sends information about repositories.
+
+    Two repository instances can be checked like so::
+
+        r1 == r2
+        r1 != r2
+
+    And is equivalent to::
+
+        r1.id == r2.id
+        r1.id != r2.id
+
