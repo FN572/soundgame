eb0760609687a58f1ca9933e50343206987b2ab2
FN572 <109976723+FN572@users.noreply.github.com>
2021-11-02 18:02:30 +0000

    add env

--- gitsome/lib/github3/repos/repo.py
+++ gitsome/lib/github3/repos/repo.py
@@ -1690,4 +1690,26 @@ class Repository(GitHubCore):
             - The ``state`` parameter now accepts 'all' in addition to 'open'
               and 'closed'.
 
-            - The ``sort`` param
+            - The ``sort`` parameter was added.
+
+            - The ``direction`` parameter was added.
+
+        :param str state: (optional), accepted values: ('all', 'open',
+            'closed')
+        :param str head: (optional), filters pulls by head user and branch
+            name in the format ``user:ref-name``, e.g., ``seveas:debian``
+        :param str base: (optional), filter pulls by base branch name.
+            Example: ``develop``.
+        :param str sort: (optional), Sort pull requests by ``created``,
+            ``updated``, ``popularity``, ``long-running``. Default: 'created'
+        :param str direction: (optional), Choose the direction to list pull
+            requests. Accepted values: ('desc', 'asc'). Default: 'desc'
+        :param int number: (optional), number of pulls to return. Default: -1
+            returns all available pull requests
+        :param str etag: (optional), ETag from a previous request to the same
+            endpoint
+        :returns: generator of
+            :class:`PullRequest <github3.pulls.PullRequest>`\ s
+        """
+        url = self._build_url('pulls', base_url=self._api)
+      
