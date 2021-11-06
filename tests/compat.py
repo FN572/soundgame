4a6a319237e673215e2ddcb37df660669accd447
FN572 <109976723+FN572@users.noreply.github.com>
2021-11-06 08:01:30 +0000

    chore(i18n): update locales

--- tests/compat.py
+++ tests/compat.py
@@ -12,3 +12,9 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
+
+import sys
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
