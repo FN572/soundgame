3170a3e19fc14c398270584672e197c5e5eb45c6
FN572 <109976723+FN572@users.noreply.github.com>
2021-11-06 19:29:10 +0000

    Update documentation/PinMultiplexor.md

---
+++ tests/data/user_feed.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2015 Donne Martin. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+formatted_user_feed = u'\x1b[35m  1.   \x1b[0mLeonardoGitHub \x1b[0m\x1b[32mforked \x1b[0m\x1b[36mdonnemartin/data-science-ipython-notebooks \x1b[0m\x1b[33m(just now)\x1b[0m\n\x1b[35m  2.   \x1b[0mnavroopsingh \x1b[0m\x1b[32mstarred\x1b[0m\x1b[36m donnemartin/dev-setup \x1b[0m\x1b[33m(just now)\x1b[0m\n\x1b[35m  3.   \x1b[0mprabaharanr \x1b[0m\x1b[32mforked \x1b[0m\x1b[36mdonnemartin/data-science-ipython-notebooks \x1b[0m\x1b[33m(just now)\x1b[0m\n\x1b[35m  4.   \x1b[0mwdavis12 \x1b[0m\x1b[32mstarred\x1b[0m\x1b[36m donnemartin/data-science-ipython-notebooks \x1b[0m\x
