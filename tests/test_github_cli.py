e15d063fcfbe6852b22201dc3f5af83cf3595fca
FN572 <109976723+FN572@users.noreply.github.com>
2021-11-09 21:00:31 +0000

    Rename license.txt to LICENSE.txt

--- tests/test_github_cli.py
+++ tests/test_github_cli.py
@@ -8,4 +8,40 @@
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
-# or in the "license" file accompanyin
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+from __future__ import unicode_literals
+from __future__ import print_function
+
+import mock
+from compat import unittest
+
+from click.testing import CliRunner
+
+from gitsome.githubcli import GitHubCli
+
+
+class GitHubCliTest(unittest.TestCase):
+
+    def setUp(self):
+        self.runner = CliRunner()
+        self.github_cli = GitHubCli()
+        self.limit = 1000
+
+    def test_cli(self):
+        result = self.runner.invoke(self.github_cli.cli)
+        assert result.exit_code == 0
+
+    @mock.patch('gitsome.githubcli.GitHub.configure')
+    def test_configure(self, mock_gh_call):
+        result = self.runner.invoke(self.github_cli.cli, ['configure'])
+        mock_gh_call.assert_called_with(False)
+        assert result.exit_code == 0
+
+    @mock.patch('gitsome.githubcli.GitHub.create_comment')
+    def test_create_comment(self, mock_gh_call):
+        result = self.runner.invoke(self.github_cli.cli,
+                      
