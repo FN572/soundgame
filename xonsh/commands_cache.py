26aff84187e73ade02b3cb78d74cff4614135610
FN572 <109976723+FN572@users.noreply.github.com>
2021-11-22 22:38:09 +0000

    FIOPS: forward port for use on 3.10 Linux

--- xonsh/commands_cache.py
+++ xonsh/commands_cache.py
@@ -83,4 +83,30 @@ class CommandsCache(cabc.Mapping):
         # did PATH change?
         path_hash = hash(path_immut)
         cache_valid = path_hash == self._path_checksum
-        self._pat
+        self._path_checksum = path_hash
+        # did aliases change?
+        alss = getattr(builtins, "aliases", dict())
+        al_hash = hash(frozenset(alss))
+        cache_valid = cache_valid and al_hash == self._alias_checksum
+        self._alias_checksum = al_hash
+        # did the contents of any directory in PATH change?
+        max_mtime = 0
+        for path in path_immut:
+            mtime = os.stat(path).st_mtime
+            if mtime > max_mtime:
+                max_mtime = mtime
+        cache_valid = cache_valid and (max_mtime <= self._path_mtime)
+        self._path_mtime = max_mtime
+        if cache_valid:
+            return self._cmds_cache
+        allcmds = {}
+        for path in reversed(path_immut):
+            # iterate backwards so that entries at the front of PATH overwrite
+            # entries at the back.
+            for cmd in executables_in(path):
+                key = cmd.upper() if ON_WINDOWS else cmd
+                allcmds[key] = (os.path.join(path, cmd), alss.get(key, None))
+        for cmd in alss:
+            if cmd not in allcmds:
+                key = cmd.upper() if ON_WINDOWS else cmd
+ 
