45b079296cc409060fc2a01d3c8fe2329af553a2
FN572 <109976723+FN572@users.noreply.github.com>
2021-11-26 14:32:01 +0000

    phpcs

--- xonsh/completers/_aliases.py
+++ xonsh/completers/_aliases.py
@@ -31,4 +31,42 @@ def _add_one_completer(name, func, loc="end"):
             new[k] = v
             if rel == k and direction == ">":
                 new[name] = func
-       
+                found = True
+        if not found:
+            new[name] = func
+    builtins.__xonsh__.completers.clear()
+    builtins.__xonsh__.completers.update(new)
+
+
+def _list_completers(args, stdin=None, stack=None):
+    o = "Registered Completer Functions: \n"
+    _comp = builtins.__xonsh__.completers
+    ml = max((len(i) for i in _comp), default=0)
+    _strs = []
+    for c in _comp:
+        if _comp[c].__doc__ is None:
+            doc = "No description provided"
+        else:
+            doc = " ".join(_comp[c].__doc__.split())
+        doc = justify(doc, 80, ml + 3)
+        _strs.append("{: >{}} : {}".format(c, ml, doc))
+    return o + "\n".join(_strs) + "\n"
+
+
+def _remove_completer(args, stdin=None, stack=None):
+    err = None
+    if len(args) != 1:
+        err = "completer remove takes exactly 1 argument."
+    else:
+        name = args[0]
+        if name not in builtins.__xonsh__.completers:
+            err = ("The name %s is not a registered " "completer function.") % name
+    if err is None:
+        del builtins.__xonsh__.completers[name]
+        return
+    else:
+        return None, err + "\n", 1
+
+
+def _register_completer(args, stdin=None, stack=None):
+    err = None
