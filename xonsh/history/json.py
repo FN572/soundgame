5f2b9da4669e1e83f32c9794cd647aeb1c157891
FN572 <109976723+FN572@users.noreply.github.com>
2022-03-11 16:55:04 +0000

    Envío PHP

--- xonsh/history/json.py
+++ xonsh/history/json.py
@@ -20,4 +20,48 @@ def _xhj_gc_commands_to_rmfiles(hsize, files):
     rmfiles = []
     n = 0
     ncmds = 0
-    for ts, fcm
+    for ts, fcmds, f in files[::-1]:
+        if fcmds == 0:
+            # we need to make sure that 'empty' history files don't hang around
+            rmfiles.append((ts, fcmds, f))
+        if ncmds + fcmds > hsize:
+            break
+        ncmds += fcmds
+        n += 1
+    rmfiles += files[:-n]
+    return rmfiles
+
+
+def _xhj_gc_files_to_rmfiles(hsize, files):
+    """Return the history files to remove to get under the file limit."""
+    rmfiles = files[:-hsize] if len(files) > hsize else []
+    return rmfiles
+
+
+def _xhj_gc_seconds_to_rmfiles(hsize, files):
+    """Return the history files to remove to get under the age limit."""
+    rmfiles = []
+    now = time.time()
+    for ts, _, f in files:
+        if (now - ts) < hsize:
+            break
+        rmfiles.append((None, None, f))
+    return rmfiles
+
+
+def _xhj_gc_bytes_to_rmfiles(hsize, files):
+    """Return the history files to remove to get under the byte limit."""
+    rmfiles = []
+    n = 0
+    nbytes = 0
+    for _, _, f in files[::-1]:
+        fsize = os.stat(f).st_size
+        if nbytes + fsize > hsize:
+            break
+        nbytes += fsize
+        n += 1
+    rmfiles = files[:-n]
+    return rmfiles
+
+
+def _xhj_g
