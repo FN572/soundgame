6ecd923d33f64772a34f3c6b62672480e26a4b76
FN572 <109976723+FN572@users.noreply.github.com>
2022-03-13 16:48:52 +0000

    fixed review comments

--- xonsh/history/json.py
+++ xonsh/history/json.py
@@ -195,4 +195,35 @@ class JsonHistoryFlusher(threading.Thread):
 
     def dump(self):
         """Write the cached history to external storage."""
-        opts = b
+        opts = builtins.__xonsh__.env.get("HISTCONTROL")
+        last_inp = None
+        cmds = []
+        for cmd in self.buffer:
+            if "ignoredups" in opts and cmd["inp"] == last_inp:
+                # Skipping dup cmd
+                continue
+            if "ignoreerr" in opts and cmd["rtn"] != 0:
+                # Skipping failed cmd
+                continue
+            cmds.append(cmd)
+            last_inp = cmd["inp"]
+        with open(self.filename, "r", newline="\n") as f:
+            hist = xlj.LazyJSON(f).load()
+        load_hist_len = len(hist["cmds"])
+        hist["cmds"].extend(cmds)
+        if self.at_exit:
+            hist["ts"][1] = time.time()  # apply end time
+            hist["locked"] = False
+        if not builtins.__xonsh__.env.get("XONSH_STORE_STDOUT", False):
+            [cmd.pop("out") for cmd in hist["cmds"][load_hist_len:] if "out" in cmd]
+        with open(self.filename, "w", newline="\n") as f:
+            xlj.ljdump(hist, f, sort_keys=True)
+
+
+class JsonCommandField(cabc.Sequence):
+    """A field in the 'cmds' portion of history."""
+
+    def __init__(self, field, hist, default=None):
+        """Represents a field in the 'cmds' portion of history.
+
+        Will query the buffer for the relevant data, if
