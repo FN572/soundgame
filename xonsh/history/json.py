5172ccf176c7399db688c3fb74fe75ea2c81e56b
FN572 <109976723+FN572@users.noreply.github.com>
2022-03-26 08:54:44 +0000

    Shorten.

--- xonsh/history/json.py
+++ xonsh/history/json.py
@@ -386,3 +386,31 @@ class JsonHistory(History):
         else:
             items = zip(self.inps, self.tss)
         for item, tss in items:
+            yield {"inp": item.rstrip(), "ts": tss[0]}
+
+    def all_items(self, newest_first=False, **kwargs):
+        """
+        Returns all history as found in XONSH_DATA_DIR.
+
+        yield format: {'inp': cmd, 'rtn': 0, ...}
+        """
+        while self.gc and self.gc.is_alive():
+            time.sleep(0.011)  # gc sleeps for 0.01 secs, sleep a beat longer
+        for f in _xhj_get_history_files(newest_first=newest_first):
+            try:
+                json_file = xlj.LazyJSON(f, reopen=False)
+            except ValueError:
+                # Invalid json file
+                continue
+            try:
+                commands = json_file.load()["cmds"]
+            except json.decoder.JSONDecodeError:
+                # file is corrupted somehow
+                if builtins.__xonsh__.env.get("XONSH_DEBUG") > 0:
+                    msg = "xonsh history file {0!r} is not valid JSON"
+                    print(msg.format(f), file=sys.stderr)
+                continue
+            if newest_first:
+                commands = reversed(commands)
+            for c in commands:
+                yield {"inp": c["inp"].rstrip(), "ts": c["ts"
