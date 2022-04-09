7cc0b6fa12be6fdfcc361e290fbb68044e8d7a35
FN572 <109976723+FN572@users.noreply.github.com>
2022-04-09 17:37:04 +0000

    Beta Release

--- xonsh/main.py
+++ xonsh/main.py
@@ -1,1 +1,38 @@
-# -*- 
+# -*- coding: utf-8 -*-
+"""The main xonsh script."""
+import os
+import sys
+import enum
+import argparse
+import builtins
+import contextlib
+import signal
+import traceback
+
+from xonsh import __version__
+from xonsh.timings import setup_timings
+from xonsh.lazyasd import lazyobject
+from xonsh.shell import Shell
+from xonsh.pretty import pretty
+from xonsh.execer import Execer
+from xonsh.proc import HiddenCommandPipeline
+from xonsh.jobs import ignore_sigtstp
+from xonsh.tools import setup_win_unicode_console, print_color, to_bool_or_int
+from xonsh.platform import HAS_PYGMENTS, ON_WINDOWS
+from xonsh.codecache import run_script_with_cache, run_code_with_cache
+from xonsh.xonfig import print_welcome_screen
+from xonsh.xontribs import xontribs_load
+from xonsh.lazyimps import pygments, pyghooks
+from xonsh.imphooks import install_import_hooks
+from xonsh.events import events
+from xonsh.environ import xonshrc_context, make_args_env
+from xonsh.built_ins import XonshSession, load_builtins, load_proxies
+
+from gitsome import __version__ as gitsome_version
+
+
+events.transmogrify("on_post_init", "LoadEvent")
+events.doc(
+    "on_post_init",
+    """
+on_post_init() -> None
