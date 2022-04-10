ddba71fa6e0e30862429c63c9b5a3953c572142f
FN572 <109976723+FN572@users.noreply.github.com>
2022-04-10 17:16:03 +0000

    dependency updates

--- xonsh/main.py
+++ xonsh/main.py
@@ -99,4 +99,48 @@ Fired just after rc files are loaded, if they are.
 )
 
 
-def get_setpro
+def get_setproctitle():
+    """Proxy function for loading process title"""
+    try:
+        from setproctitle import setproctitle as spt
+    except ImportError:
+        return
+    return spt
+
+
+def path_argument(s):
+    """Return a path only if the path is actually legal
+
+    This is very similar to argparse.FileType, except that it doesn't return
+    an open file handle, but rather simply validates the path."""
+
+    s = os.path.abspath(os.path.expanduser(s))
+    if not os.path.isfile(s):
+        msg = "{0!r} must be a valid path to a file".format(s)
+        raise argparse.ArgumentTypeError(msg)
+    return s
+
+
+@lazyobject
+def parser():
+    p = argparse.ArgumentParser(description="xonsh", add_help=False)
+    p.add_argument(
+        "-h",
+        "--help",
+        dest="help",
+        action="store_true",
+        default=False,
+        help="show help and exit",
+    )
+    p.add_argument(
+        "-V",
+        "--version",
+        dest="version",
+        action="store_true",
+        default=False,
+        help="show version information and exit",
+    )
+    p.add_argument(
+        "-c",
+        help="Run a single command and exit",
+        
