8875ea47338578da79cd8d184a11a6e67d87ccec
FN572 <109976723+FN572@users.noreply.github.com>
2022-05-21 09:29:24 +0000

    Fix null pointer

--- xonsh/parsers/context_check.py
+++ xonsh/parsers/context_check.py
@@ -26,4 +26,43 @@ def _not_assignable(x, augassign=False):
         return "function call"
     elif isinstance(x, ast.Lambda):
         return "lambda"
-    eli
+    elif isinstance(x, (ast.BoolOp, ast.BinOp, ast.UnaryOp)):
+        return "operator"
+    elif isinstance(x, ast.IfExp):
+        return "conditional expression"
+    elif isinstance(x, ast.ListComp):
+        return "list comprehension"
+    elif isinstance(x, ast.DictComp):
+        return "dictionary comprehension"
+    elif isinstance(x, ast.SetComp):
+        return "set comprehension"
+    elif isinstance(x, ast.GeneratorExp):
+        return "generator expression"
+    elif isinstance(x, ast.Compare):
+        return "comparison"
+    elif isinstance(x, ast.Name) and x.id in _all_keywords:
+        return "keyword"
+    elif isinstance(x, ast.NameConstant):
+        return "keyword"
+
+
+_loc = collections.namedtuple("_loc", ["lineno", "column"])
+
+
+def check_contexts(tree):
+    c = ContextCheckingVisitor()
+    c.visit(tree)
+    if c.error is not None:
+        e = SyntaxError(c.error[0])
+        e.loc = _loc(c.error[1], c.error[2])
+        raise e
+
+
+class ContextCheckingVisitor(ast.NodeVisitor):
+    def __init__(self):
+        self.error = None
+
+    def visit_Delete(self, node):
+        for i in node.targets:
+            err = _not_assignable(i)
+            if err is 
