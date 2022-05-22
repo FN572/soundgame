7f6f69da446e808cd4cbdf4cc3a2266300cfdb32
FN572 <109976723+FN572@users.noreply.github.com>
2022-05-22 12:25:23 +0000

    added video link

--- xonsh/parsers/v35.py
+++ xonsh/parsers/v35.py
@@ -40,4 +40,44 @@ class Parser(BaseParser):
             lexer_table=lexer_table,
             yacc_optimize=yacc_optimize,
             yacc_table=yacc_table,
-            yacc_debug=yacc_deb
+            yacc_debug=yacc_debug,
+            outputdir=outputdir,
+        )
+
+    def p_classdef_or_funcdef(self, p):
+        """classdef_or_funcdef : classdef
+                               | funcdef
+                               | async_funcdef
+        """
+        p[0] = p[1]
+
+    def p_async_funcdef(self, p):
+        """async_funcdef : async_tok funcdef"""
+        p1, f = p[1], p[2][0]
+        p[0] = [ast.AsyncFunctionDef(**f.__dict__)]
+        p[0][0]._async_tok = p1
+
+    def p_async_compound_stmt(self, p):
+        """compound_stmt : async_stmt"""
+        p[0] = p[1]
+
+    def p_async_for_stmt(self, p):
+        """async_for_stmt : ASYNC for_stmt"""
+        f = p[2][0]
+        p[0] = [ast.AsyncFor(**f.__dict__)]
+
+    def p_async_with_stmt(self, p):
+        """async_with_stmt : ASYNC with_stmt"""
+        w = p[2][0]
+        p[0] = [ast.AsyncWith(**w.__dict__)]
+
+    def p_atom_expr_await(self, p):
+        """atom_expr : await_tok atom trailer_list_opt"""
+        p0 = self.apply_trailers(p[2], p[3])
+        p1 = p[1]
+        p0 = ast.Await(value=p0, ctx=ast.Load(), lineno=p1.lineno, col_offset=p1.lexpos)
+        p[0] = p0
+
+    def p_async_stmt(self, p):
+        """async_stmt : async_funcdef
+          
