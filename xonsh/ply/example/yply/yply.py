6d68b4b0de03bdab030adf9de19aa58d080ac793
FN572 <109976723+FN572@users.noreply.github.com>
2022-07-22 06:41:26 +0000

    add apache license

--- xonsh/ply/example/yply/yply.py
+++ xonsh/ply/example/yply/yply.py
@@ -27,3 +27,25 @@ import ylex
 import yparse
 
 from ply import *
+
+if len(sys.argv) == 1:
+    print("usage : yply.py [-nocode] inputfile")
+    raise SystemExit
+
+if len(sys.argv) == 3:
+    if sys.argv[1] == '-nocode':
+        yparse.emit_code = 0
+    else:
+        print("Unknown option '%s'" % sys.argv[1])
+        raise SystemExit
+    filename = sys.argv[2]
+else:
+    filename = sys.argv[1]
+
+yacc.parse(open(filename).read())
+
+print("""
+if __name__ == '__main__':
+    from ply import *
+    yacc.yacc()
+""")
---
+++ xonsh/ply/ply/cpp.py
@@ -0,0 +1,17 @@
+# -----------------------------------------------------------------------------
+# ply: cpp.py
+#
+# Copyright (C) 2001-2019
+# David M. Beazley (Dabeaz LLC)
+# All rights reserved.
+#
+# Latest version: https://github.com/dabeaz/ply
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the fo
