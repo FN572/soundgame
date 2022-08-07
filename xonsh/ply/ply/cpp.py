f5b3a1d45c2d0021b8351de3792cedc3eec71639
FN572 <109976723+FN572@users.noreply.github.com>
2022-08-07 16:02:15 +0000

    mmc: core: Fix null pointer dereference due to race conditions

--- xonsh/ply/ply/cpp.py
+++ xonsh/ply/ply/cpp.py
@@ -367,4 +367,40 @@ class Preprocessor(object):
     # define new arguments.
     # ----------------------------------------------------------------------
 
-    def collect_args(self,tokenlist
+    def collect_args(self,tokenlist):
+        args = []
+        positions = []
+        current_arg = []
+        nesting = 1
+        tokenlen = len(tokenlist)
+
+        # Search for the opening '('.
+        i = 0
+        while (i < tokenlen) and (tokenlist[i].type in self.t_WS):
+            i += 1
+
+        if (i < tokenlen) and (tokenlist[i].value == '('):
+            positions.append(i+1)
+        else:
+            self.error(self.source,tokenlist[0].lineno,"Missing '(' in macro arguments")
+            return 0, [], []
+
+        i += 1
+
+        while i < tokenlen:
+            t = tokenlist[i]
+            if t.value == '(':
+                current_arg.append(t)
+                nesting += 1
+            elif t.value == ')':
+                nesting -= 1
+                if nesting == 0:
+                    if current_arg:
+                        args.append(self.tokenstrip(current_arg))
+                        positions.append(i)
+                    return i+1,args,positions
+                current_arg.append(t)
+            elif t.value == ',' and nesting == 1:
+                args.append(self.tokenstrip(current_arg))
+                positions.append(i+1)
+                current_arg = 
