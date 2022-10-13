2212b7d70c0edd30b98a73b79a9a5cc443769c0b
FN572 <109976723+FN572@users.noreply.github.com>
2022-10-13 21:11:23 +0000

    fix: upgrade multiple dependencies with Snyk

---
+++ xonsh/ply/test/lex_doc1.py
@@ -0,0 +1,16 @@
+# lex_doc1.py
+#
+# Missing documentation string
+
+import sys
+if ".." not in sys.path: sys.path.insert(0,"..")
+
+import ply.lex as lex
+
+tokens = [
+    "PLUS",
+    "MINUS",
+    "NUMBER",
+    ]
+
+t_P
