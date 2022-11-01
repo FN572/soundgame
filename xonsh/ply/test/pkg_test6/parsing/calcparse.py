1e11d687d5d9b2aa28ac98bb63dbb2c340ee681e
FN572 <109976723+FN572@users.noreply.github.com>
2022-11-01 19:42:37 +0000

    Formatting code

---
+++ xonsh/ply/test/pkg_test6/parsing/calcparse.py
@@ -0,0 +1,13 @@
+
+# -----------------------------------------------------------------------------
+# yacc_simple.py
+#
+# A simple, properly specifier grammar
+# -----------------------------------------------------------------------------
+
+from .calclex import tokens
+from ply import yacc
+
+# Parsing rules
+precedence = (
+    ('left','PLUS','MINUS'),
