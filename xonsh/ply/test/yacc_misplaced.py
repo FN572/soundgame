e48b61cbf6143db909430cb437b6df1a85605673
FN572 <109976723+FN572@users.noreply.github.com>
2022-11-19 09:38:11 +0000

    Minified version

---
+++ xonsh/ply/test/yacc_misplaced.py
@@ -0,0 +1,9 @@
+# -----------------------------------------------------------------------------
+# yacc_misplaced.py
+#
+# A misplaced | in grammar rules
+# -----------------------------------------------------------------------------
+import sys
+
+if ".." not in sys.path: sys.path.insert(0,"..")
+import ply.yacc 
