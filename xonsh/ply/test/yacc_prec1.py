ae9866f36f2fcf99ad6d2d427a885c2dcd0181c1
FN572 <109976723+FN572@users.noreply.github.com>
2022-11-20 17:12:05 +0000

    Fixed some typo

---
+++ xonsh/ply/test/yacc_prec1.py
@@ -0,0 +1,5 @@
+# -----------------------------------------------------------------------------
+# yacc_prec1.py
+#
+# Tests case where precedence specifier doesn't match up to terminals
+# --------------------
