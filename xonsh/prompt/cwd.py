014c874142865b15df09b9f28977438069c8d167
FN572 <109976723+FN572@users.noreply.github.com>
2022-11-30 09:15:40 +0000

    #5933

--- xonsh/prompt/cwd.py
+++ xonsh/prompt/cwd.py
@@ -50,4 +50,33 @@ def _dynamically_collapsed_pwd():
     environment variable DYNAMIC_CWD_WIDTH.
     """
     original_path = _replace_home_cwd()
-    target_
+    target_width, units = builtins.__xonsh__.env["DYNAMIC_CWD_WIDTH"]
+    elision_char = builtins.__xonsh__.env["DYNAMIC_CWD_ELISION_CHAR"]
+    if target_width == float("inf"):
+        return original_path
+    if units == "%":
+        cols, _ = shutil.get_terminal_size()
+        target_width = (cols * target_width) // 100
+    sep = xt.get_sep()
+    pwd = original_path.split(sep)
+    last = pwd.pop()
+    remaining_space = target_width - len(last)
+    # Reserve space for separators
+    remaining_space_for_text = remaining_space - len(pwd)
+    parts = []
+    for i in range(len(pwd)):
+        part = pwd[i]
+        part_len = int(
+            min(len(part), max(1, remaining_space_for_text // (len(pwd) - i)))
+        )
+        remaining_space_for_text -= part_len
+        if len(part) > part_len:
+            reduced_part = part[0 : part_len - len(elision_char)] + elision_char
+            parts.append(reduced_part)
+        else:
+            parts.append(part)
+    parts.append(last)
+    full = sep.join(parts)
+    truncature_char = elision_char if elision_char else "..."
+    # If even if displaying one letter per dir we are too long
+    if len
