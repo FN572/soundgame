f9a8374a34b0c0e14f00b157182e96edc06504b7
FN572 <109976723+FN572@users.noreply.github.com>
2022-12-04 22:43:33 +0000

    add .npmignore

--- xonsh/prompt/gitstatus.py
+++ xonsh/prompt/gitstatus.py
@@ -6,4 +6,44 @@ import collections
 import os
 import subprocess
 
-import xo
+import xonsh.lazyasd as xl
+
+
+GitStatus = collections.namedtuple(
+    "GitStatus",
+    [
+        "branch",
+        "num_ahead",
+        "num_behind",
+        "untracked",
+        "changed",
+        "conflicts",
+        "staged",
+        "stashed",
+        "operations",
+    ],
+)
+
+
+def _check_output(*args, **kwargs):
+    kwargs.update(
+        dict(
+            env=builtins.__xonsh__.env.detype(),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.DEVNULL,
+            universal_newlines=True,
+        )
+    )
+    timeout = builtins.__xonsh__.env["VC_BRANCH_TIMEOUT"]
+    # See https://docs.python.org/3/library/subprocess.html#subprocess.Popen.communicate
+    with subprocess.Popen(*args, **kwargs) as proc:
+        try:
+            out, err = proc.communicate(timeout=timeout)
+            if proc.returncode != 0:
+                raise subprocess.CalledProcessError(
+                    proc.returncode, proc.args, output=out, stderr=err
+                )  # note err will always be empty as we redirect stderr to DEVNULL abvoe
+            return out
+        except subprocess.TimeoutExpired:
+            # We use `.terminate()` (SIGTERM) instead of `.kill()` (SIGKILL) here
+            # because other
