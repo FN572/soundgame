de8229b3e53cd42ce5a5d567bfbe06e7a4e87bd5
FN572 <109976723+FN572@users.noreply.github.com>
2022-12-06 15:45:34 +0000

    Working prototype

--- xonsh/prompt/gitstatus.py
+++ xonsh/prompt/gitstatus.py
@@ -116,4 +116,31 @@ def gitstatus():
     branch name, number of ahead commit, number of behind commit,
     untracked number, changed number, conflicts number,
     staged number, stashed number, operation."""
-    status = _check_output(["git", "status", "--porcelain", "--br
+    status = _check_output(["git", "status", "--porcelain", "--branch"])
+    branch = ""
+    num_ahead, num_behind = 0, 0
+    untracked, changed, conflicts, staged = 0, 0, 0, 0
+    for line in status.splitlines():
+        if line.startswith("##"):
+            line = line[2:].strip()
+            if "Initial commit on" in line:
+                branch = line.split()[-1]
+            elif "no branch" in line:
+                branch = _get_tag_or_hash()
+            elif "..." not in line:
+                branch = line
+            else:
+                branch, rest = line.split("...")
+                if " " in rest:
+                    divergence = rest.split(" ", 1)[-1]
+                    divergence = divergence.strip("[]")
+                    for div in divergence.split(", "):
+                        if "ahead" in div:
+                            num_ahead = int(div[len("ahead ") :].strip())
+                        elif "behind" in div:
+                            num_behind = int(div[len("behind ") :].strip())
+        elif line.startswith("??"):
+            untracked += 1
+        else:
+            if len(line) > 1 and line[1] == "M":
+  
