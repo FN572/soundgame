bd006459317a13c575940dcb46d03b8790794587
FN572 <109976723+FN572@users.noreply.github.com>
2022-12-21 16:46:48 +0000

    download happend

--- xonsh/pytest_plugin.py
+++ xonsh/pytest_plugin.py
@@ -30,4 +30,36 @@ def _limited_traceback(excinfo):
 
 
 def pytest_collect_file(parent, path):
-    if path.ext.lower() == ".xsh" and path.basename.startsw
+    if path.ext.lower() == ".xsh" and path.basename.startswith("test_"):
+        return XshFile(path, parent)
+
+
+class XshFile(pytest.File):
+    def collect(self):
+        sys.path.append(self.fspath.dirname)
+        mod = importlib.import_module(self.fspath.purebasename)
+        sys.path.pop(0)
+        tests = [t for t in dir(mod) if t.startswith("test_")]
+        for test_name in tests:
+            obj = getattr(mod, test_name)
+            if hasattr(obj, "__call__"):
+                yield XshFunction(
+                    name=test_name, parent=self, test_func=obj, test_module=mod
+                )
+
+
+class XshFunction(pytest.Item):
+    def __init__(self, name, parent, test_func, test_module):
+        super().__init__(name, parent)
+        self._test_func = test_func
+        self._test_module = test_module
+
+    def runtest(self, *args, **kwargs):
+        self._test_func(*args, **kwargs)
+
+    def repr_failure(self, excinfo):
+        """ called when self.runtest() raises an exception. """
+        formatted_tb = _limited_traceback(excinfo)
+        formatted_tb.insert(0, "xonsh execution failed\n")
+        formatted_tb.append("{}: {}".format(excinfo.type.__name__, excinfo.value))
+        return "
