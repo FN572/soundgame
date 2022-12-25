0bfd7ddd9ec3258c039b32dd37752baeb6c4ea80
FN572 <109976723+FN572@users.noreply.github.com>
2022-12-25 07:11:36 +0000

    updated function

--- xonsh/tracer.py
+++ xonsh/tracer.py
@@ -10,4 +10,43 @@ import functools
 
 from xonsh.lazyasd import LazyObject
 from xonsh.platform import HAS_PYGMENTS
-from xonsh.tools import DefaultNotGiven, print_color,
+from xonsh.tools import DefaultNotGiven, print_color, normabspath, to_bool
+from xonsh.inspectors import find_file, getouterframes
+from xonsh.lazyimps import pygments, pyghooks
+from xonsh.proc import STDOUT_CAPTURE_KINDS
+import xonsh.prompt.cwd as prompt
+
+terminal = LazyObject(
+    lambda: importlib.import_module("pygments.formatters.terminal"),
+    globals(),
+    "terminal",
+)
+
+
+class TracerType(object):
+    """Represents a xonsh tracer object, which keeps track of all tracing
+    state. This is a singleton.
+    """
+
+    _inst = None
+    valid_events = frozenset(["line", "call"])
+
+    def __new__(cls, *args, **kwargs):
+        if cls._inst is None:
+            cls._inst = super(TracerType, cls).__new__(cls, *args, **kwargs)
+        return cls._inst
+
+    def __init__(self):
+        self.prev_tracer = DefaultNotGiven
+        self.files = set()
+        self.usecolor = True
+        self.lexer = pyghooks.XonshLexer()
+        self.formatter = terminal.TerminalFormatter()
+        self._last = ("", -1)  # filename, lineno tuple
+
+    def __del__(self):
+        for f in set(self.files):
+            self.stop(f)
+
+    def color_output(self, usecolor):
+        """Specify whether or not the tracer output shoul
