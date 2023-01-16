72d6b280ff272a2552ea14d37376edef8cdb9dc0
FN572 <109976723+FN572@users.noreply.github.com>
2023-01-16 21:56:12 +0000

    Project started

--- xonsh/xoreutils/which.py
+++ xonsh/xoreutils/which.py
@@ -1,4 +1,48 @@
 """Implements the which xoreutil."""
 import os
 import argparse
-import built
+import builtins
+import functools
+
+from xonsh.xoreutils import _which
+import xonsh.platform as xp
+import xonsh.proc as xproc
+
+
+@functools.lru_cache()
+def _which_create_parser():
+    desc = "Parses arguments to which wrapper"
+    parser = argparse.ArgumentParser("which", description=desc)
+    parser.add_argument(
+        "args", type=str, nargs="+", help="The executables or aliases to search for"
+    )
+    parser.add_argument(
+        "-a",
+        "--all",
+        action="store_true",
+        dest="all",
+        help="Show all matches in globals, xonsh.aliases, $PATH",
+    )
+    parser.add_argument(
+        "-s",
+        "--skip-alias",
+        action="store_true",
+        help="Do not search inxonsh.aliases",
+        dest="skip",
+    )
+    parser.add_argument(
+        "-V",
+        "--version",
+        action="version",
+        version="{}".format(_which.__version__),
+        help="Display the version of the python which module " "used by xonsh",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        dest="verbose",
+        help="Print out how matches were located and show " "near misses on stderr",
+    )
+    parser.add_argument(
+        "-
